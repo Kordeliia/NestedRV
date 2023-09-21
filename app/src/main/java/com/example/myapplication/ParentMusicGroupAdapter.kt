df21fb6cf0c7bd6fb8401f00aa21957f2dd5e3d9
Kordeliia <lalvmun@hotmail.com>
2023-09-21 16:41:18 +0200

    Commit ListAdapter del padre

---
+++ app/src/main/java/com/example/myapplication/ParentMusicGroupAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.databinding.ItemParentMusicGroupBinding
+
+class ParentMusicGroupAdapter: ListAdapter<MusicGroup, RecyclerView.ViewHolder>(MusicGroupDiffCallback()) {
+    private lateinit var context : Context
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.item_parent_music_group, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val musicGroup = getItem(position)
+        with(holder as ViewHolder){
+            binding.tvParent.text = musicGroup.name
+        }
+    }
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val binding = ItemParentMusicGroupBinding.bind(view)
+    }
+    class MusicGroupDiffCallback: DiffUtil.ItemCallback<MusicGroup>(){
+        override fun areItemsTheSame(oldItem: MusicGroup, newItem: MusicGroup)= oldItem.name == newItem.name
+        override fun areContentsTheSame(oldItem: MusicGroup, newItem: MusicGroup) = oldItem == newItem
+
+    }
+
+}
