7ca420ace3ce872d0b9c389364f77ca19f6206cf
Kordeliia <lalvmun@hotmail.com>
2023-09-21 17:02:20 +0200

    commit recyclerView del padre

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var parentAdapter: ParentMusicGroupAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
+        getData()
+    }
+
+    private fun setupRecyclerView() {
+        parentAdapter = ParentMusicGroupAdapter()
+        binding.rvVistaPpal.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = parentAdapter
+        }
+    }
+
+    private fun getData() {
+        TODO("Not yet implemented")
     }
 }
